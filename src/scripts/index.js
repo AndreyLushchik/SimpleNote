4e3468b0487f848b2a0497f112a8508384ae7267
AndreyLushchik <redbull199556@gmail.com>
2021-09-29 23:52:33 +0300

    create main html and css

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+/*window.addEventListener('DOMContentLoaded',() =>{
+  window.setInterval(function(){
+  let now = new Date();
+  let clock = document.getElementById("clock");
+  clock.innerHTML = now.toLocaleTimeString();
+  },1000);
+});*/
+import {clock} from './clock.js'
+setInterval(clock, 1000);
