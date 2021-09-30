08394701a3ab6c5fe0ef89ab34c07eadb1d57d3b
Romanova-Anastasia <aoonabara@gmail.com>
2021-09-30 15:31:44 +0300

    init: add modal_window by button add todo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,5 +5,13 @@
   clock.innerHTML = now.toLocaleTimeString();
   },1000);
 });*/
-import {clock} from './clock.js'
+
+import { clock } from "./clock.js";
 setInterval(clock, 1000);
+
+import { openModal, closeModal} from "./modal_window.js"
+
+
+
+
+
