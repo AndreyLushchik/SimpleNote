46f702ceb8ba05fdd956ffb6ffa9cbd88df1bd15
Romanova-Anastasia <aoonabara@gmail.com>
2021-10-04 15:44:43 +0300

    save

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import { clock } from "./clock.js";
-import { openModal, closeModal } from "./modal_window.js";
+import { createHeader } from "./modal_window.js";
 import {
   dropdownIcon,
   dropdown,
@@ -8,4 +8,30 @@ import {
   toggleDropdown,
   selectOption,
 } from "./modal_windows-select";
+
+//Start App
+document.addEventListener("DOMContentLoaded", app);
+
+function app() {
+  const root = document.getElementById("root");
+
+
+  //openModalBtn.addEventListener("click", openModal);
+  //modal.addEventListener("click", closeModal);
+  const header = createHeader();
+  header.addEventListener("click", onHeaderClick);
+
+  root.append(header); //вставляет переданный элемент после дочернего
+
+}
+
+// Event Handlers
+function onHeaderClick(event) {
+  if ((event.target.id = ".js-open-modal")) {
+    openModalBtn.addEventListener("click", openModal);
+  } else if ((event.target.id = ".js-close-modal")) {
+    modal.addEventListener("click", closeModal);
+  }
+}
+
 setInterval(clock, 1000);
--- src/scripts/modal_window.js
+++ src/scripts/modal_window.js
@@ -2,16 +2,34 @@ const openModalBtn = document.querySelector(".js-open-modal");
 const closeModalBtn = document.querySelector(".js-close-modal");
 const modal = document.querySelector(".js-modal-backdrop");
 
+/*
 openModalBtn.addEventListener("click", openModal);
-modal.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);*/
 
-function openModal() {
-  modal.classList.remove("modal-hidden");
+function createElement(tag, className, text = "") {
+  const element = document.createElement(tag);
+  element.className = className;
+
+  const textElement = document.createTextNode(text);
+  element.append(textElement);
+
+  return element;
 }
 
-function closeModal(event) {
-  if (event.target === modal || event.target === closeModalBtn)
-  modal.classList.add("modal-hidden");
+function createHeader() {
+  const header = createElement();
+
+  function openModal() {
+    modal.classList.remove("modal-hidden");
+  }
+
+  function closeModal(event) {
+    if (event.target === modal || event.target === closeModalBtn)
+      modal.classList.add("modal-hidden");
+  }
+
+  header.append(openModal, closeModal);
+  return header;
 }
 
-export { openModal, closeModal };
+export { createHeader };
