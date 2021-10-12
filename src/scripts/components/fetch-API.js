0ef7901671522888f9c1efc6a4c7d98934789b91
Sergey <sergeihar82@gmail.com>
2021-10-12 15:26:46 +0200

    refactoring: Edited files fetch API and local-storage.js

--- src/scripts/components/fetch-API.js
+++ src/scripts/components/fetch-API.js
@@ -3,6 +3,7 @@ import{printUsers} from "./../index.js"
 export function getUsers() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((response) => response.forEach(printUsers));
+      .then((response) => response.forEach(printUsers))
+      .catch(e => console.log(`Error! ${e}`))
   }
 
