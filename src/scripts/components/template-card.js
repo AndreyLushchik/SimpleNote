0d241f8cbf3fb8949c8ec0f0e6d2c0b23481b753
Sergey <sergeihar82@gmail.com>
2021-10-14 20:13:31 +0200

    feat: implemented the drag & drop function

--- src/scripts/components/template-card.js
+++ src/scripts/components/template-card.js
@@ -1,5 +1,5 @@
 
-import{asynchronous } from "../index.js";
+import{asynchronous, renderCards} from "../index.js";
 import{setDataLocalStorage,getDataLocalStorage} from "./local-storage.js";
 import{CreatePopup} from "./template-popup";
 
@@ -16,7 +16,7 @@ export function CreateCard({id, title, description, user, time}) {
   const cardBtnEdit = document.querySelector(".card__btn-edit");
   const cardBtnBack = document.querySelector(".card__btn-back")
 
-  this.template = function () {
+    this.template = function () {
     return `<div class="card" draggable="true" id="${id}">
     <h3 class="card__title">${title}</h3>
     <input disabled type="textarea" placeholder="Todo" class="card__description" value=${description}>
@@ -30,12 +30,12 @@ export function CreateCard({id, title, description, user, time}) {
     </div>`;
   }
 
-  this.printCard = function (item) {
+    this.printCard = function (item) {
     const card = this.template();
     item.insertAdjacentHTML("afterbegin", card);
   };
-  
-  this.addListeners = function () {
+
+    this.addListeners = function () {
     const card = document.querySelectorAll(".card");
     card.forEach(item => {
       item.addEventListener("click", this.eventHandling);
@@ -43,55 +43,59 @@ export function CreateCard({id, title, description, user, time}) {
       item.addEventListener("dragend", this.dragEnd);
     })
 };
- 
 
-this.eventHandling = function (event) {
-  switch(event.target){
- case cardBtnProgress:
- cardContainerProgress.append(card)
- const addProgressClass = getDataLocalStorage()
- addProgressClass.map(item => (item.id == card.id) && (item.classeCard = "progress") )
-     setDataLocalStorage(addProgressClass )
-     cardBtnEdit.classList.add("hidden")
-     cardBtnBack.classList.add("show")
-     cardBtnDelete.classList.add("hidden")
-     cardBtnComlete.classList.add("show")
-     cardBtnProgress.classList.add("hidden")  
-     break;
-  case cardBtnDelete:
-   card.remove();
-   const deleteData = getDataLocalStorage().filter( item  => item.id != card.id);
-   setDataLocalStorage(deleteData)
-   break;
-   case cardBtnEdit: 
-   getDataLocalStorage().map(item => item.id == card.id && new CreatePopup(item).popupOpen())
-   break;
-   case cardBtnBack:
-   cardContainerTodo.append(card);
-   const addTodoClass = getDataLocalStorage()
-   addTodoClass.map(item => (item.id == card.id) && (item.classeCard = "todo"))
-     setDataLocalStorage(addTodoClass)
-     cardBtnEdit.classList.remove("hidden")
-     cardBtnBack.classList.remove("show")
-     cardBtnDelete.classList.remove("hidden")
-     cardBtnComlete.classList.remove("show")
-     cardBtnProgress.classList.remove("hidden")
-     break;
-   case cardBtnComlete:
-   cardContainerDone.append(card);
-   const addDoneClass = getDataLocalStorage()
-   addDoneClass.map(item =>(item.id == card.id) && (item.classeCard = "done"))
-   setDataLocalStorage(addDoneClass)
-   cardBtnBack.classList.remove("show")
-   cardBtnComlete.classList.remove("show")
-   cardBtnDelete.classList.remove("hidden")
-   break;
- }
-}
+  this.eventHandling = function (event) {
+     switch(event.target){
+    case cardBtnProgress:
+    const lengthProgresse = getDataLocalStorage().filter(item => item.classeCard === "progress") 
+    if(lengthProgresse.length >= 5){
+    alert("it is necessary to complete the current todo")
+  } else{
+    const addProgressClass = getDataLocalStorage()  
+    addProgressClass.map(item => item.id == card.id && (item.classeCard = "progress"))
+    cardContainerProgress.append(card);
+    setDataLocalStorage(addProgressClass )
+    cardBtnEdit.classList.add("hidden")
+    cardBtnBack.classList.add("show")
+    cardBtnDelete.classList.add("hidden")
+    cardBtnComlete.classList.add("show")
+    cardBtnProgress.classList.add("hidden")  
+  }
+        break;
+     case cardBtnDelete:
+      card.remove();
+      const deleteData = getDataLocalStorage().filter( item  => item.id != card.id);
+      setDataLocalStorage(deleteData)
+      break;
+      case cardBtnEdit: 
+      getDataLocalStorage().map(item => item.id == card.id && new CreatePopup(item).popupOpen())
+      break;
+      case cardBtnBack:
+      cardContainerTodo.append(card);
+      const addTodoClass = getDataLocalStorage()
+      addTodoClass.map(item => (item.id == card.id) && (item.classeCard = "todo"))
+        setDataLocalStorage(addTodoClass)
+        cardBtnEdit.classList.remove("hidden")
+        cardBtnBack.classList.remove("show")
+        cardBtnDelete.classList.remove("hidden")
+        cardBtnComlete.classList.remove("show")
+        cardBtnProgress.classList.remove("hidden")
+        break;
+      case cardBtnComlete:
+      cardContainerDone.append(card);
+      const addDoneClass = getDataLocalStorage()
+      addDoneClass.map(item =>(item.id == card.id) && (item.classeCard = "done"))
+      setDataLocalStorage(addDoneClass)
+      cardBtnBack.classList.remove("show")
+      cardBtnComlete.classList.remove("show")
+      cardBtnDelete.classList.remove("hidden")
+      break;
+    }
+   }
 
   // drag drop
 
-  this.dragStart = function(card){
+  this.dragStart = function(e){
           const drop = document.querySelectorAll(".drop")
           drop.forEach(item =>{
             item.addEventListener("dragover", this.dragOver)
@@ -99,84 +103,47 @@ this.eventHandling = function (event) {
             item.addEventListener("dragleave", this.dragLeave)
             item.addEventListener("drop",this.dragDrop)
           })
-          asynchronous(0).then(()=> card.target.style.display = "none")
-
+          asynchronous(0).then(()=> e.target.style.display = "none")
+         
           this.dragDrop = function (event){
-          if(event.target === cardContainerTodo ){
-          
-            //event.target.append(card.target)
-            console.log(card.target)
-            const  data =  getDataLocalStorage()
-            data.map((item) =>{
-              if(item.id == card.target.id){
-                item.classeCard = "todo"
-              
-              }
-              setDataLocalStorage(data);
-           
-              card.target.style.background = "blue"
-              cardBtnEdit.classList.remove("hidden")
-              cardBtnBack.classList.remove("show")
-              cardBtnDelete.classList.remove("hidden")
-              cardBtnComlete.classList.remove("show")
-              cardBtnProgress.classList.remove("hidden")
-              event.target.append(card.target)
-            })
-          } else if(event.target === cardContainerProgress){
-           
-            //event.target.append(card.target)
-            console.log(card.target)
-            const data =  getDataLocalStorage();
-            data.map(item => {
-              if (item.id == card.target.id) {
-                item.classeCard = "progress"
-
-              }
-              setDataLocalStorage(data)
-             
-              card.target.style.background = "green"
-              cardBtnEdit.classList.add("hidden")
-              cardBtnBack.classList.add("show")
-              cardBtnDelete.classList.add("hidden")
-              cardBtnComlete.classList.add("show")
-              cardBtnProgress.classList.add("hidden")
-              event.target.append(card.target)
-            })
-          } else if(event.target ===  cardContainerDone ){
-           
-            //event.target.append(card.target)
-            console.log(card.target)
-            const data =  getDataLocalStorage()
-            data.map(item => {
-              if (item.id == card.target.id) {
-                item.classeCard = "done"
-               
-              }
-              setDataLocalStorage(data)
-           
-              card.target.style.background = "red"
-              cardBtnBack.classList.remove("show")
-              cardBtnComlete.classList.remove("show")
-              cardBtnDelete.classList.remove("hidden")
-              event.target.append(card.target)
-            })
+          switch(event.target){
+          case cardContainerTodo:
+          const addClasseTodo = getDataLocalStorage()
+          addClasseTodo.map(item => (item.id == e.target.id) && (item.classeCard = "todo"))
+          setDataLocalStorage(addClasseTodo)
+          renderCards(addClasseTodo)
+          break;   
+          case cardContainerProgress:
+            const lengthProgresse = getDataLocalStorage().filter(item => item.classeCard === "progress") 
+            if(lengthProgresse.length >= 5){
+            alert("it is necessary to complete the current todo")
+          } else{
+            const addProgressClass = getDataLocalStorage()  
+            addProgressClass.map(item => item.id == card.id && (item.classeCard = "progress"))
+            cardContainerProgress.append(card);
+            setDataLocalStorage(addProgressClass )
+            renderCards(addProgressClass)
           }
+          break;   
+          case cardContainerDone:
+          const addClasseDone = getDataLocalStorage()
+          addClasseDone.map(item =>(item.id == e.target.id) && (item.classeCard = "done"))
+          setDataLocalStorage(addClasseDone)
+          renderCards(addClasseDone)
+          break;
         }
+      }
        this.dragOver = function(event){
-       event.preventDefault()
-       } 
+      event.preventDefault()
+      } 
        this.dragEnter = function(event){
-  
-       event.target.classList.add("drag-enter")
-       }
+      event.target.classList.add("drag-enter")
+      }
        this.dragLeave = function(event){
-        event.target.classList.remove("drag-enter")
+      event.target.classList.remove("drag-enter")
       }
     }
-    
-      this.dragEnd = function(event){
-      event.target.style.display = "block"
+    this.dragEnd = function(event){
+    event.target.style.display = "block"
+     }
     }
-    
-  //---------------------------------------------------------
-  }
