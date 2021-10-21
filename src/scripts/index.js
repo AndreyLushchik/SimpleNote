941f5d160d2e894acc66f1f231964a24fa08e781
AndreyLushchik <86970947+AndreyLushchik@users.noreply.github.com>
2021-10-21 16:44:31 +0300

    Delete src directory

--- src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/*window.addEventListener('DOMContentLoaded',() =>{
-  window.setInterval(function(){
-  let now = new Date();
-  let clock = document.getElementById("clock");
-  clock.innerHTML = now.toLocaleTimeString();
-  },1000);
-});*/
-import {clock} from './clock.js'
-setInterval(clock, 1000);
